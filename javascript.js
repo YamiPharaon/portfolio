1f4fe6a948a92c46d593ec3887b56820ba3f8d75
YamiPharaon <47097650+YamiPharaon@users.noreply.github.com>
2020-07-25 10:43:23 +0200

    First commit
    Ajout des fichiers

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var compteur=0;
+
+function close_fenetre() {
+
+
+    var random = Math.round(Math.random() * 10);
+    
+    alert("Arrête de vouloir fermer tous ce que tu vois tu es insupportable !" + random);
+
+}
+
